7f1882a08d40bbe76ca1dd43273c4558522e6fbf
Fani Fadillah Hermawan <65802784+fanifh11@users.noreply.github.com>
2020-05-23 14:57:54 +0700

    Upload file dashboard

---
+++ DashboardVisual/DashboardVisual/GradienPanel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DashboardVisual
+{
+    class GradientPanel : Panel
+    {
+        public Color ColorTop { get; set; }
+        public Color ColorBottom { get; set; }
+
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            LinearGradientBrush lgb = new LinearGradientBrush(this.ClientRectangle, this.ColorBottom, this.ColorTop, 90F);
+            Graphics g = e.Graphics;
+            g.FillRectangle(lgb, this.ClientRectangle);
+            base.OnPaint(e);
+
+        }
+
+    }
+}
